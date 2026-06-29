bdb81b3a67d6999280a4d5aa7dbe401de291f1a5
unknown <arzuanon@gmail.com>
2026-06-30 00:06:37 +0600

    change from main

--- index.java
+++ index.java
@@ -1,5 +1,5 @@
 public static void main(String args[]){
-    System.out.println("HELLO");
+    System.out.println("HELLO main");
     int n=5;
     for(int i=1;i<=n;i++){
         System.out.println("i");
