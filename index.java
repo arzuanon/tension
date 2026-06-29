95d36ff83666d74fee0a7870500bb78389adfffb
unknown <arzuanon@gmail.com>
2026-06-30 00:12:20 +0600

    resolve merge conflict

--- index.java
+++ index.java
@@ -1,9 +1,9 @@
 public static void main(String args[]){
-<<<<<<< HEAD
+
     System.out.println("HELLO main");
-=======
-    System.out.println("HELLO branch1");
->>>>>>> branch1
+
+    
+
     int n=5;
     for(int i=1;i<=n;i++){
         System.out.println("i");
